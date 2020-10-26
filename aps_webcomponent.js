49a7b39c3eb461a813ece77f6484a992b28496ed
evgeny.grin@iprosis.com <evgeny.grin@iprosis.com>
2020-10-26 15:10:12 +0200

    progress with tutorial

--- aps_webcomponent.js
+++ aps_webcomponent.js
@@ -58,7 +58,7 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
-
+            redraw();
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
