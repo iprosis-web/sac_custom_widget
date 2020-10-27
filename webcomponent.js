6a0ada2737b32ce352167708c32a8800d55bdb92
Erika Grin <evgeny.grin@iprosis.com>
2020-10-27 18:34:02 +0200

    tutorial progress

--- webcomponent.js
+++ webcomponent.js
@@ -1,84 +1,86 @@
-(function()  {
-    let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
+(function () {
+	let tmpl = document.createElement('template');
+	tmpl.innerHTML = `
     `;
 
-    customElements.define('com-sap-sample-helloworld2', class WidgetTemplate extends HTMLElement {
+	customElements.define(
+		'com-sap-sample-helloworld2',
+		class WidgetTemplate extends HTMLElement {
+			constructor() {
+				super();
+				this._shadowRoot = this.attachShadow({ mode: 'open' });
+				this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+				this._tagContainer = null;
+				this._firstConnection = true;
+				this._tagType = 'h3';
+				this._tagText = 'Hello World h3';
 
+				//Adding event handler for click events
+				this.addEventListener('click', (event) => {
+					var event = new Event('onClick');
+					this.dispatchEvent(event);
+				});
+			}
 
-		constructor() {
-			super(); 
-			this._shadowRoot = this.attachShadow({mode: "open"});
-            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this._tagContainer = null;
-            this._tagType = 'h3';
-            this._tagText = 'Hello World h3';
-		}
+			//Fired when the widget is added to the html DOM of the page
+			connectedCallback() {
+				this._firstConnection = true;
+				this.redraw();
+			}
 
-        //Fired when the widget is added to the html DOM of the page
-        connectedCallback(){
-            this._firstConnection = true;
-            this.redraw();
-        }
+			//Fired when the widget is removed from the html DOM of the page (e.g. by hide)
+			disconnectedCallback() {}
 
-         //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
-        disconnectedCallback(){
-        
-        }
+			//When the custom widget is updated, the Custom Widget SDK framework executes this function first
+			onCustomWidgetBeforeUpdate(oChangedProperties) {}
 
-         //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-		onCustomWidgetBeforeUpdate(oChangedProperties) {
-		}
+			//When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
+			onCustomWidgetAfterUpdate(oChangedProperties) {
+				this.redraw();
+			}
 
-        //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-		onCustomWidgetAfterUpdate(oChangedProperties) {
-                this.redraw();
-        }
-        
-        //When the custom widget is removed from the canvas or the analytic application is closed
-        onCustomWidgetDestroy(){
-        }
+			//When the custom widget is removed from the canvas or the analytic application is closed
+			onCustomWidgetDestroy() {}
 
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-        /*
+			//When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
+			// Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
+			//  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
+			/*
         onCustomWidgetResize(width, height){
         
         }
         */
 
-        redraw() {
-            if (this._tagText) {
-                if (this._tagContainer) {
-                    this._tagContainer.parentNode.removeChild(this._tagContainer);
-                }
+			redraw() {
+				if (this._tagText) {
+					if (this._tagContainer) {
+						this._tagContainer.parentNode.removeChild(this._tagContainer);
+					}
 
-                this._tagContainer = document.createElement(this._tagType);
-                let theText = document.createTextNode(this._tagText);
-                this._tagContainer.appendChild(theText);
-                this._shadowRoot.appendChild(this._tagContainer);
-            }
-        }
+					this._tagContainer = document.createElement(this._tagType);
+					let theText = document.createTextNode(this._tagText);
+					this._tagContainer.appendChild(theText);
+					this._shadowRoot.appendChild(this._tagContainer);
+				}
+			}
 
-        //Getters ande Setters
+			//Getters ande Setters
 
-        get widgetText() {
-            return this._tagType;
-        }
+			get widgetText() {
+				return this._tagType;
+			}
 
-        set widgetText(value) {
-            this._tagText = value;
-        }
+			set widgetText(value) {
+				this._tagText = value;
+			}
 
-        get headingType() {
-            return this._tagType;
-        }
+			get headingType() {
+				return this._tagType;
+			}
 
-        set headingType(value) {
-            this._tagType = value;
-        }
-        
-    });
-        
-}) ();
+			set headingType(value) {
+				this._tagType = value;
+			}
+		}
+	);
+})();
